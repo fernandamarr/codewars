7f0ca667e250f413edc19bd2734697318758adf4
Fernanda <fernanda.marroquin07@gmail.com>
2019-07-04 11:16:23 -0400

    convert binary

---
+++ javascript/8kyu/convert-to-binary.js
@@ -0,0 +1,15 @@
+// Given a non-negative integer n, write a function toBinary/ToBinary which returns that number in a binary format.
+
+// to_binary(1)  /* should return 1 */
+// to_binary(5)  /* should return 101 */
+// to_binary(11) /* should return 1011 */
+// Example:
+
+// to_binary(1)  /* should return 1 */
+// to_binary(5)  /* should return 101 */
+// to_binary(11) /* should return 1011 */
+
+function toBinary(n) {
+    let str = n.toString(2);
+    return parseInt(str);
+}
