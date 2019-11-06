86ffe3756b2c9735b2b17043f449cb0d2f2ab7cc
Fernanda <fernanda.marroquin07@gmail.com>
2019-11-06 10:44:15 -0500

    integral updates

--- javascript/8kyu/integral.js
+++ javascript/8kyu/integral.js
@@ -3,7 +3,7 @@
 // In order to find the integral all you need to do is add one to the exponent (the second argument), and divide the coefficient (the first argument) by that new number.
 
 function integrate(coefficient, exponent) {
-  let newExponent = exponent + 1;
-  let newCoefficient = coefficient / newExponent;
-  return newCoefficient + "x^" + newExponent;
+  let x = exponent + 1;
+  let y = coefficient / x;
+  return `${y}x^${x}`
 }
