429e8794bd3dcec915339f125a7d049a67bf618d
Fernanda <fernanda.marroquin07@gmail.com>
2019-11-06 10:39:34 -0500

    integral solved

---
+++ javascript/8kyu/integral.js
@@ -0,0 +1,9 @@
+// Create a function that finds the integral of the expression passed.
+
+// In order to find the integral all you need to do is add one to the exponent (the second argument), and divide the coefficient (the first argument) by that new number.
+
+function integrate(coefficient, exponent) {
+  let newExponent = exponent + 1;
+  let newCoefficient = coefficient / newExponent;
+  return newCoefficient + "x^" + newExponent;
+}
