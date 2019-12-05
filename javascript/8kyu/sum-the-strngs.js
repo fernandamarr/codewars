6388679fd23bb66ec9823b87947257f85d255c20
Fernanda <fernanda.marroquin07@gmail.com>
2019-12-05 13:19:16 -0500

    sum strings solved

---
+++ javascript/8kyu/sum-the-strngs.js
@@ -0,0 +1,6 @@
+// Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
+
+function sumStr(a,b) {
+	let add = Number(a) + Number(b);
+	return add.toString()
+}
